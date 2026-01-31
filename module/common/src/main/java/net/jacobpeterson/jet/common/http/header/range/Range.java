4e18e2a44f47267e7edf9d899dd7894b73278c14
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-01-31 15:00:58 -0700

    Add `Range` with test

---
+++ module/common/src/main/java/net/jacobpeterson/jet/common/http/header/range/Range.java
@@ -0,0 +1,265 @@
+package net.jacobpeterson.jet.common.http.header.range;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import com.google.errorprone.annotations.Immutable;
+import com.google.errorprone.annotations.concurrent.LazyInit;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import net.jacobpeterson.jet.common.http.header.Header;
+import net.jacobpeterson.jet.common.http.header.contentrange.ContentRange;
+import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
+
+import java.util.Collection;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.Long.parseLong;
+import static lombok.EqualsAndHashCode.CacheStrategy.LAZY;
+
+/**
+ * {@link Range} is an immutable class that represents a standardized HTTP {@link Header#RANGE}.
+ * <p>
+ * The HTTP <strong><code>Range</code></strong>
+ * <a href="https://developer.mozilla.org/en-US/docs/Glossary/Request_header">request header</a> indicates the part of
+ * a resource that the server should return. Several parts can be requested at the same time in one <code>Range</code>
+ * header, and the server may send back these ranges in a multipart document. If the server sends back ranges, it uses
+ * the
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/206"><code>206 Partial Content</code></a>
+ * status code for the response. If the ranges are invalid, the server returns the
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/416"><code>416 Range Not
+ * Satisfiable</code></a> error.
+ * <p>
+ * A server that doesn't support range requests may ignore the <code>Range</code> header and return the whole resource
+ * with a <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/200"><code>200</code></a> status
+ * code. Older browsers used a response header of
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Accept-Ranges"><code>Accept-Ranges:
+ * none</code></a> to disable features like 'pause' or 'resume' in download managers, but since a server ignoring the
+ * <code>Range</code> header has the same meaning as responding with <code>Accept-Ranges: none</code>, the header is
+ * rarely used in this way.
+ * <p>
+ * Currently only
+ * <a href="https://www.iana.org/assignments/http-parameters/http-parameters.xhtml#range-units"><code>bytes</code> units
+ * are registered</a> which are <em>offsets</em> (zero-indexed &amp; inclusive). If the requested data has a
+ * <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Content-Encoding">content coding</a>
+ * applied, each byte range represents the encoded sequence of bytes, not the bytes that would be obtained after
+ * decoding.
+ * <p>
+ * The header is a
+ * <a href="https://developer.mozilla.org/en-US/docs/Glossary/CORS-safelisted_request_header">CORS-safelisted request
+ * header</a> when the directive specifies a single byte range.
+ *
+ * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range">developer.mozilla.org</a>
+ * @see Header#RANGE
+ * @see ContentRange
+ */
+@NullMarked
+@Immutable
+@EqualsAndHashCode(cacheStrategy = LAZY)
+public final class Range {
+
+    /**
+     * The bytes {@link #getUnit()}: <code>"bytes"</code>
+     */
+    public static final String BYTES_UNIT = "bytes";
+
+    /**
+     * The {@link #getUnit()}-range delimiter: <code>"="</code>
+     */
+    public static final String UNIT_RANGE_DELIMITER = "=";
+
+    /**
+     * The {@link #getStart()}-{@link #getEnd()} delimiter: <code>"-"</code>
+     */
+    public static final String START_END_DELIMITER = "-";
+
+    /**
+     * The multiple ranges delimiter: <code>","</code>
+     *
+     * @see #multipleToString(Collection)
+     */
+    public static final String MULTIPLE_RANGES_DELIMITER = ",";
+
+    /**
+     * A {@link Range} with {@link #getUnit()} set to {@link #BYTES_UNIT}, {@link #getStart()} set to <code>0</code>,
+     * and {@link #getEnd()} set to <code>null</code>.
+     */
+    public static final Range ALL_BYTES = builder().unit(BYTES_UNIT).start(0L).build();
+
+    private static final Splitter PARSE_UNIT_RANGE_SPLITTER =
+            Splitter.on(UNIT_RANGE_DELIMITER).limit(2).trimResults().omitEmptyStrings();
+    private static final Splitter PARSE_RANGES_SPLITTER =
+            Splitter.on(MULTIPLE_RANGES_DELIMITER).trimResults().omitEmptyStrings();
+    private static final Splitter PARSE_START_END_SPLITTER =
+            Splitter.on(START_END_DELIMITER).limit(2).trimResults();
+
+    /**
+     * Parses the given {@link Header#RANGE} value {@link String} into a {@link ImmutableList} of {@link Range}s.
+     *
+     * @param range the {@link Header#RANGE} value {@link String}
+     *
+     * @return the {@link Range} {@link ImmutableList}
+     *
+     * @throws IllegalArgumentException thrown upon parsing failure
+     * @see #toString()
+     */
+    public static ImmutableList<Range> parse(final String range) throws IllegalArgumentException {
+        final var ranges = ImmutableList.<Range>builder();
+        final var unitRangeSplit = PARSE_UNIT_RANGE_SPLITTER.splitToList(range);
+        checkArgument(unitRangeSplit.size() == 2, "Invalid range: %s", range);
+        final var unit = unitRangeSplit.getFirst();
+        for (final var rangeSplit : PARSE_RANGES_SPLITTER.split(unitRangeSplit.get(1))) {
+            final var startEndSplit = PARSE_START_END_SPLITTER.splitToList(rangeSplit);
+            checkArgument(startEndSplit.size() == 2, "Invalid range: %s", range);
+            final var builder = builder().unit(unit);
+            final var start = startEndSplit.getFirst();
+            final var end = startEndSplit.get(1);
+            try {
+                if (!start.isEmpty()) {
+                    builder.start(parseLong(start));
+                }
+                if (!end.isEmpty()) {
+                    builder.end(parseLong(end));
+                }
+            } catch (final NumberFormatException numberFormatException) {
+                throw new IllegalArgumentException(numberFormatException);
+            }
+            ranges.add(builder.build());
+        }
+        return ranges.build();
+    }
+
+    /**
+     * Creates a {@link Header#RANGE} value {@link String} from the given {@link Range} {@link Collection}.
+     *
+     * @param ranges the {@link Range} {@link Collection}. If {@link Collection#isEmpty()}, then
+     *               {@link Range#toString()} of {@link #ALL_BYTES} is returned.
+     *
+     * @return the {@link Header#RANGE} value {@link String}
+     */
+    public static String multipleToString(final Collection<Range> ranges) {
+        if (ranges.isEmpty()) {
+            return ALL_BYTES.toString();
+        }
+        final var string = new StringBuilder();
+        var first = true;
+        for (final var range : ranges) {
+            if (first) {
+                string.append(range);
+                first = false;
+            } else {
+                string.append(MULTIPLE_RANGES_DELIMITER).append(' ').append(range.toStringNoUnit());
+            }
+        }
+        return string.toString();
+    }
+
+    /**
+     * {@link Builder} is a builder class for {@link Range}.
+     *
+     * @see #builder()
+     */
+    public static final class Builder {}
+
+    /**
+     * The unit in which ranges are defined. Currently only <code>bytes</code> are a registered unit.
+     *
+     * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range#unit">
+     * developer.mozilla.org</a>
+     * @see #BYTES_UNIT
+     */
+    private final @Getter String unit;
+
+    /**
+     * An integer in the given unit indicating the start position of the request range.
+     *
+     * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range#range-start">
+     * developer.mozilla.org</a>
+     */
+    private final @Getter @Nullable Long start;
+
+    /**
+     * If {@link #getStart()} is non-<code>null</code>:
+     * <ul>
+     * <li>An integer in the given unit indicating the end position of the requested range. This value is optional and,
+     * if omitted, the end of the resource is used as the end of the range.</li>
+     * </ul>
+     * If {@link #getStart()} is <code>null</code>:
+     * <ul>
+     * <li>An integer indicating the number of units at the end of the resource to return.</li>
+     * </ul>
+     *
+     * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range#range-end">
+     * developer.mozilla.org</a>
+     * @see <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Range#suffix-length">
+     * developer.mozilla.org</a>
+     */
+    private final @Getter @Nullable Long end;
+
+    private @LazyInit @EqualsAndHashCode.Exclude @Nullable String stringNoUnit;
+    private @LazyInit @EqualsAndHashCode.Exclude @Nullable String string;
+
+    /**
+     * @param unit  {@link #getUnit()}, or <code>null</code> for {@link #BYTES_UNIT}
+     * @param start {@link #getStart()}, or possibly <code>null</code> if {@link #getEnd()} is non-<code>null</code>
+     * @param end   {@link #getEnd()}, or possibly <code>null</code> if {@link #getStart()} is non-<code>null</code>
+     *
+     * @throws IllegalArgumentException thrown if {@link #getStart()} or {@link #getEnd()} are invalid
+     */
+    @lombok.Builder(toBuilder = true)
+    private Range(final @Nullable String unit, @Nullable final Long start, @Nullable final Long end)
+            throws IllegalArgumentException {
+        checkArgument(start != null || end != null, "`start` and `end` cannot both be `null`");
+        if (start != null) {
+            checkArgument(start >= 0, "`start` must be positive");
+        }
+        if (end != null) {
+            checkArgument(end >= 0, "`end` must be positive");
+        }
+        if (start != null && end != null) {
+            checkArgument(start <= end, "`start` must be less than or equal to `end`");
+        }
+        this.unit = unit == null ? BYTES_UNIT : unit;
+        this.start = start;
+        this.end = end;
+    }
+
+    /**
+     * @return internally-cached {@link String} value for {@link Header#RANGE}, but without the prefixed
+     * {@link #getUnit()} and {@link #UNIT_RANGE_DELIMITER}
+     */
+    public String toStringNoUnit() {
+        if (stringNoUnit == null) {
+            final var stringNoUnit = new StringBuilder();
+            appendToStringNoUnit(stringNoUnit);
+            this.stringNoUnit = stringNoUnit.toString();
+        }
+        return stringNoUnit;
+    }
+
+    private void appendToStringNoUnit(final StringBuilder string) {
+        if (start != null) {
+            string.append(start);
+        }
+        string.append(START_END_DELIMITER);
+        if (end != null) {
+            string.append(end);
+        }
+    }
+
+    /**
+     * @return internally-cached {@link String} value for {@link Header#RANGE}
+     *
+     * @see #parse(String)
+     */
+    @Override
+    public String toString() {
+        if (string == null) {
+            final var string = new StringBuilder();
+            string.append(unit).append(UNIT_RANGE_DELIMITER);
+            appendToStringNoUnit(string);
+            this.string = string.toString();
+        }
+        return string;
+    }
+}
---
+++ module/common/src/test/java/net/jacobpeterson/jet/common/http/header/range/RangeTest.java
@@ -0,0 +1,170 @@
+package net.jacobpeterson.jet.common.http.header.range;
+
+import org.jspecify.annotations.NullMarked;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@NullMarked
+public class RangeTest {
+
+    @Test
+    public void parse() {
+        {
+            final var ranges = Range.parse("bytes=0-");
+            assertEquals(1, ranges.size());
+            final var range0 = ranges.getFirst();
+            assertEquals("bytes", range0.getUnit());
+            assertEquals(0, range0.getStart());
+            assertNull(range0.getEnd());
+        }
+        {
+            final var ranges = Range.parse("bits= -1");
+            assertEquals(1, ranges.size());
+            final var range0 = ranges.getFirst();
+            assertEquals("bits", range0.getUnit());
+            assertNull(range0.getStart());
+            assertEquals(1, range0.getEnd());
+        }
+        {
+            final var ranges = Range.parse(" bytes =  0 -1");
+            assertEquals(1, ranges.size());
+            final var range0 = ranges.getFirst();
+            assertEquals("bytes", range0.getUnit());
+            assertEquals(0, range0.getStart());
+            assertEquals(1, range0.getEnd());
+        }
+        {
+            final var ranges = Range.parse("bytes  =0 - 1 , -2");
+            assertEquals(2, ranges.size());
+
+            final var range0 = ranges.getFirst();
+            assertEquals("bytes", range0.getUnit());
+            assertEquals(0, range0.getStart());
+            assertEquals(1, range0.getEnd());
+
+            final var range1 = ranges.get(1);
+            assertEquals("bytes", range1.getUnit());
+            assertNull(range1.getStart());
+            assertEquals(2, range1.getEnd());
+        }
+        {
+            final var ranges = Range.parse("bytes  =1 - 4 , 8-9,,");
+            assertEquals(2, ranges.size());
+
+            final var range0 = ranges.getFirst();
+            assertEquals("bytes", range0.getUnit());
+            assertEquals(1, range0.getStart());
+            assertEquals(4, range0.getEnd());
+
+            final var range1 = ranges.get(1);
+            assertEquals("bytes", range1.getUnit());
+            assertEquals(8, range1.getStart());
+            assertEquals(9, range1.getEnd());
+        }
+        assertThrows(IllegalArgumentException.class, () -> Range.parse(""));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse(" "));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("="));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("= - "));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("b= - "));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=0"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=a"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=-a"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=-1,-"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=0-1,0-,="));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=--1"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=-1-"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=-1--2"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=-1--2"));
+        assertThrows(IllegalArgumentException.class, () -> Range.parse("bytes=2-1"));
+    }
+
+    @Test
+    public void multipleToString() {
+        assertEquals("bytes=0-", Range.multipleToString(List.of()));
+        assertEquals("bytes=0-1", Range.multipleToString(Range.parse("bytes=0-1")));
+        assertEquals("bytes=0-1, 2-3, -4", Range.multipleToString(Range.parse("bytes=0-1, 2-3, -4")));
+    }
+
+    public static final class BuilderTest {
+
+        @Test
+        public void unit() {
+            assertThrows(IllegalArgumentException.class, () -> Range.builder().build());
+            assertThrows(IllegalArgumentException.class, () -> Range.builder().unit("bits").build());
+            assertEquals("bits", Range.builder()
+                    .unit("bits")
+                    .start(0L)
+                    .build().getUnit());
+            assertEquals(Range.BYTES_UNIT, Range.builder()
+                    .unit(Range.BYTES_UNIT)
+                    .start(0L)
+                    .build().getUnit());
+            assertEquals(Range.BYTES_UNIT, Range.builder()
+                    .start(0L)
+                    .build().getUnit());
+        }
+
+        @Test
+        public void start() {
+            assertThrows(IllegalArgumentException.class, () -> Range.builder().start(-1L).build());
+            assertEquals(1, Range.builder()
+                    .start(1L)
+                    .build().getStart());
+        }
+
+        @Test
+        public void end() {
+            assertThrows(IllegalArgumentException.class, () -> Range.builder().end(-1L).build());
+            assertEquals(1, Range.builder()
+                    .end(1L)
+                    .build().getEnd());
+        }
+    }
+
+    @Test
+    public void toStringNoUnit() {
+        assertEquals("0-", Range.builder()
+                .start(0L)
+                .build().toStringNoUnit());
+        assertEquals("-1", Range.builder()
+                .end(1L)
+                .build().toStringNoUnit());
+        assertEquals("0-1", Range.builder()
+                .start(0L)
+                .end(1L)
+                .build().toStringNoUnit());
+    }
+
+    @Test
+    public void _toString() {
+        assertEquals("bytes=0-", Range.builder()
+                .start(0L)
+                .build().toString());
+        assertEquals("bytes=-1", Range.builder()
+                .end(1L)
+                .build().toString());
+        assertEquals("bytes=0-1", Range.builder()
+                .start(0L)
+                .end(1L)
+                .build().toString());
+
+        assertEquals("bits=0-", Range.builder()
+                .unit("bits")
+                .start(0L)
+                .build().toString());
+        assertEquals("bits=-1", Range.builder()
+                .unit("bits")
+                .end(1L)
+                .build().toString());
+        assertEquals("bits=0-1", Range.builder()
+                .unit("bits")
+                .start(0L)
+                .end(1L)
+                .build().toString());
+    }
+}
