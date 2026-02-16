f2c1d6c7c6673c900b3027c697a871d5261b0aa6
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-02-16 00:33:28 -0700

    Add `OpenApiSchemaJsonSerializer`

---
+++ module/openapi-annotations/plugin/src/main/java/net/jacobpeterson/jet/openapiannotations/plugin/gson/serializer/openapischema/OpenApiSchemaJsonSerializer.java
@@ -0,0 +1,73 @@
+package net.jacobpeterson.jet.openapiannotations.plugin.gson.serializer.openapischema;
+
+import com.github.victools.jsonschema.generator.SchemaGenerator;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import net.jacobpeterson.jet.openapiannotations.annotation.OpenApiSchema;
+import org.jspecify.annotations.NullMarked;
+import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.node.ObjectNode;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * {@link OpenApiSchemaJsonSerializer} is a {@link JsonSerializer} for {@link OpenApiSchema}.
+ */
+@NullMarked
+@RequiredArgsConstructor
+public class OpenApiSchemaJsonSerializer implements JsonSerializer<OpenApiSchema> {
+
+    private static final ObjectMapper JACKSON_OBJECT_MAPPER = new ObjectMapper();
+
+    private final SchemaGenerator schemaGenerator;
+
+    @Override
+    public JsonElement serialize(final OpenApiSchema src, final Type typeOfSrc,
+            final JsonSerializationContext context) {
+        final var from = src.from();
+        if (from.length == 0) {
+            return serializeAsAnnotationClass(context, src);
+        }
+        checkArgument(from.length == 1, "`@OpenApiSchema.from` cannot contain more than one `Class`");
+        final var generatedSchema = schemaGenerator.generateSchema(from[0]);
+        final var value = src.value();
+        if (!value.isEmpty()) {
+            final var valueJsonNode = JACKSON_OBJECT_MAPPER.readTree(value);
+            checkArgument(valueJsonNode.isObject(), "`@OpenApiSchema.value` must be a JSON object");
+            generatedSchema.setAll((ObjectNode) valueJsonNode);
+        }
+        return serializeAsAnnotationClass(context, new OpenApiSchemaWrapper(generatedSchema.toString()));
+    }
+
+    private JsonElement serializeAsAnnotationClass(final JsonSerializationContext context, final OpenApiSchema src) {
+        return context.serialize(src, Annotation.class); // Prevent recursion by specifying `Annotation.class` as type
+    }
+
+    @Value
+    @SuppressWarnings({"ClassExplicitlyAnnotation", "ImmutableAnnotationChecker"})
+    private static class OpenApiSchemaWrapper implements OpenApiSchema {
+
+        String value;
+
+        @Override
+        public String value() {
+            return value;
+        }
+
+        @Override
+        public Class<?>[] from() {
+            return new Class[]{};
+        }
+
+        @Override
+        public Class<? extends Annotation> annotationType() {
+            return OpenApiSchema.class;
+        }
+    }
+}
