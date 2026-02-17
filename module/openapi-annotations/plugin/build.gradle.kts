c6968bbf77fcc66d0fd46bce1f6a670723266f8b
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-02-17 15:03:29 -0700

    Add `signing` plugin and publishing details to `gradlePlugin` in `openapi-annotations:plugin` module

--- module/openapi-annotations/plugin/build.gradle.kts
+++ module/openapi-annotations/plugin/build.gradle.kts
@@ -1,6 +1,12 @@
+import org.jreleaser.model.api.signing.Signing.GPG_PASSPHRASE
+import org.jreleaser.model.api.signing.Signing.GPG_SECRET_KEY
+import org.jreleaser.util.Env.JRELEASER_ENV_PREFIX
+import java.lang.System.getenv
+
 plugins {
     id("module-common")
     `java-gradle-plugin`
+    signing
     id("io.github.gmazzo.gradle.testkit.jacoco") version "1.0.5"
     id("com.gradle.plugin-publish") version "2.0.0"
 }
@@ -15,7 +21,26 @@ dependencies {
     api("com.networknt:json-schema-validator:3.0.0")
 }
 
+val projectDescription = "A code-first OpenAPI specification annotations processor Gradle plugin."
+
+gradlePlugin {
+    plugins.create("openApiAnnotationsPlugin") {
+        id = "net.jacobpeterson.jet.openapiannotations.plugin"
+        implementationClass = "net.jacobpeterson.jet.openapiannotations.plugin.OpenApiAnnotationsPlugin"
+        displayName = "Jet OpenAPI Annotations Plugin"
+        description = projectDescription
+        website = "https://$GITHUB_PROJECT_DOMAIN_PATH"
+        vcsUrl = website.map { "$it.git" }
+        tags = listOf("jet", "openapi", "annotations")
+    }
+}
+
 publishing {
-    publications.getByName<MavenPublication>("maven").pom.description =
-            "A code-first OpenAPI specification annotations processor Gradle plugin."
+    publications.getByName<MavenPublication>("maven").pom.description = projectDescription
+}
+
+signing {
+    useInMemoryPgpKeys(getenv(JRELEASER_ENV_PREFIX + GPG_SECRET_KEY),
+            getenv(JRELEASER_ENV_PREFIX + GPG_PASSPHRASE))
+    sign(publishing.publications)
 }
