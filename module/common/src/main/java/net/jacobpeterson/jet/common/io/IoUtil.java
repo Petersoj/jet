5d440e86fa7c266433d6bbe38d7a309376abf40c
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-03-03 23:40:38 -0700

    Add `IoUtil` with test

---
+++ module/common/src/main/java/net/jacobpeterson/jet/common/io/IoUtil.java
@@ -0,0 +1,108 @@
+package net.jacobpeterson.jet.common.io;
+
+import com.google.errorprone.annotations.Immutable;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+
+import static java.lang.Math.min;
+import static java.nio.ByteBuffer.wrap;
+import static java.nio.charset.CodingErrorAction.REPORT;
+import static lombok.AccessLevel.PRIVATE;
+
+/**
+ * {@link IoUtil} is utility class for I/O.
+ */
+@NullMarked
+public final class IoUtil {
+
+    /**
+     * @return {@link #isLikelyPlainText(Charset, byte[], int, int)} with <code>offset</code> set to <code>0</code> and
+     * <code>length</code> set to {@link Math#min(int, int)} of <code>peekLength</code> and <code>bytes.length</code>
+     */
+    public static boolean isLikelyPlainText(final Charset charset, final byte[] bytes, final int peekLength) {
+        return isLikelyPlainText(charset, bytes, 0, min(bytes.length, peekLength));
+    }
+
+    /**
+     * @return {@link #isLikelyPlainText(Charset, byte[], int, int)} with <code>offset</code> set to <code>0</code> and
+     * <code>length</code> set to <code>bytes.length</code>
+     */
+    public static boolean isLikelyPlainText(final Charset charset, final byte[] bytes) {
+        return isLikelyPlainText(charset, bytes, 0, bytes.length);
+    }
+
+    /**
+     * Checks if the given <code>bytes</code> is likely plain text in the given {@link Charset}.
+     *
+     * @param charset the {@link Charset} of the plain text
+     * @param bytes   the bytes
+     * @param offset  the <code>bytes</code> offset to check
+     * @param length  the <code>bytes</code> length to check
+     *
+     * @return <code>true</code> for plain text likely, <code>false</code> otherwise
+     */
+    public static boolean isLikelyPlainText(final Charset charset,
+            final byte[] bytes, final int offset, final int length) {
+        final var decoder = charset.newDecoder();
+        decoder.onMalformedInput(REPORT);
+        decoder.onUnmappableCharacter(REPORT);
+        try {
+            final var decoded = decoder.decode(wrap(bytes, offset, length));
+            while (decoded.hasRemaining()) {
+                final var c = decoded.get();
+                if (c != '\n' && c != '\r' && c != '\t' && (c < 0x20 || c == 0x7F)) {
+                    return false;
+                }
+            }
+        } catch (final CharacterCodingException characterCodingException) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Peeks bytes from the given {@link InputStream}.
+     * <p>
+     * Note: the returned {@link Peeked#getInputStream()} must be used for future read operations and
+     * the given {@link InputStream} must no longer be used directly.
+     *
+     * @param inputStream the {@link InputStream}
+     * @param peekLength  the peek length
+     *
+     * @return the {@link Peeked}
+     *
+     * @throws IOException thrown for {@link IOException}s
+     */
+    public static Peeked peekInputStream(final InputStream inputStream, final int peekLength) throws IOException {
+        final var peekedBytesRead = inputStream.readNBytes(peekLength);
+        return new Peeked(new SequenceInputStream(new ByteArrayInputStream(peekedBytesRead), inputStream),
+                peekedBytesRead);
+    }
+
+    /**
+     * {@link Peeked} is a class for the return type of {@link #peekInputStream(InputStream, int)}.
+     */
+    @Immutable
+    @RequiredArgsConstructor(access = PRIVATE) @Getter
+    public static final class Peeked {
+
+        /**
+         * The {@link InputStream} given to {@link #peekInputStream(InputStream, int)}, but with {@link #getBytes()}
+         * prepended using {@link SequenceInputStream}.
+         */
+        private final InputStream inputStream;
+
+        /** The peeked bytes. */
+        private final byte[] bytes;
+    }
+
+    private IoUtil() {}
+}
---
+++ module/common/src/test/java/net/jacobpeterson/jet/common/io/IoUtilTest.java
@@ -0,0 +1,51 @@
+package net.jacobpeterson.jet.common.io;
+
+import org.jspecify.annotations.NullMarked;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static java.nio.charset.StandardCharsets.UTF_16;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@NullMarked
+public final class IoUtilTest {
+
+    @Test
+    public void isLikelyPlainTextPeekLength() {
+        assertTrue(IoUtil.isLikelyPlainText(UTF_8, "abc".getBytes(UTF_8), 2));
+        assertFalse(IoUtil.isLikelyPlainText(UTF_8, new byte[]{0, 1, 2, 3}, 2));
+    }
+
+    @Test
+    public void isLikelyPlainText() {
+        assertTrue(IoUtil.isLikelyPlainText(UTF_8, "".getBytes(UTF_8)));
+        assertTrue(IoUtil.isLikelyPlainText(UTF_8, "abc".getBytes(UTF_8)));
+        assertTrue(IoUtil.isLikelyPlainText(UTF_8, "abc\r\n\t😂".getBytes(UTF_8)));
+        assertTrue(IoUtil.isLikelyPlainText(UTF_16, "abc".getBytes(UTF_16)));
+        assertFalse(IoUtil.isLikelyPlainText(UTF_8, new byte[]{0, 1, 2, 3}));
+    }
+
+    @Test
+    public void peekInputStream() throws IOException {
+        {
+            final var peeked = IoUtil.peekInputStream(new ByteArrayInputStream(new byte[]{0, 1, 2, 3}), 2);
+            assertArrayEquals(new byte[]{0, 1, 2, 3}, peeked.getInputStream().readAllBytes());
+            assertArrayEquals(new byte[]{0, 1}, peeked.getBytes());
+        }
+        {
+            final var peeked = IoUtil.peekInputStream(new ByteArrayInputStream(new byte[]{0, 1, 2, 3}), 4);
+            assertArrayEquals(new byte[]{0, 1, 2, 3}, peeked.getInputStream().readAllBytes());
+            assertArrayEquals(new byte[]{0, 1, 2, 3}, peeked.getBytes());
+        }
+        {
+            final var peeked = IoUtil.peekInputStream(new ByteArrayInputStream(new byte[]{0, 1, 2, 3}), 5);
+            assertArrayEquals(new byte[]{0, 1, 2, 3}, peeked.getInputStream().readAllBytes());
+            assertArrayEquals(new byte[]{0, 1, 2, 3}, peeked.getBytes());
+        }
+    }
+}
