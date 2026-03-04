be4063ee8103dc5ab4d0ed000d2a4f0e48f5d252
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-03-04 12:42:43 -0700

    Move Brotli4j and Zstd-jni compression dependencies from `module:server` to `module:common`

--- module/common/build.gradle.kts
+++ module/common/build.gradle.kts
@@ -5,6 +5,17 @@ plugins {
 dependencies {
     // `jetty-client` depends on `jetty-server`, so including this dependency in the `common` module is fine.
     implementation("org.eclipse.jetty:jetty-server:12.1.6")
+
+    setOf("brotli4j",
+            "native-linux-x86_64", "native-linux-aarch64",
+            "native-osx-x86_64", "native-osx-aarch64",
+            "native-windows-x86_64", "native-windows-aarch64")
+            .forEach { implementation("com.aayushatharva.brotli4j:${it}:1.20.0") }
+
+    setOf("linux_amd64", "linux_aarch64",
+            "darwin_x86_64", "darwin_aarch64",
+            "win_amd64", "win_aarch64")
+            .forEach { implementation("com.github.luben:zstd-jni:1.5.7-6:${it}") }
 }
 
 publishing {
--- module/server/build.gradle.kts
+++ module/server/build.gradle.kts
@@ -7,19 +7,6 @@ dependencies {
 
     api("com.github.ben-manes.caffeine:caffeine:3.2.3")
 
-    val brotliDependencyGroup = "com.aayushatharva.brotli4j"
-    setOf("brotli4j",
-            "native-linux-x86_64", "native-linux-aarch64",
-            "native-osx-x86_64", "native-osx-aarch64",
-            "native-windows-x86_64", "native-windows-aarch64")
-            .forEach { implementation("${brotliDependencyGroup}:${it}:1.20.0") }
-
-    val zstdDependencyGroup = "com.github.luben"
-    setOf("linux_amd64", "linux_aarch64",
-            "darwin_x86_64", "darwin_aarch64",
-            "win_amd64", "win_aarch64")
-            .forEach { implementation("${zstdDependencyGroup}:zstd-jni:1.5.7-6:${it}") }
-
     val jettyVersion = "12.1.6"
     implementation("org.eclipse.jetty:jetty-alpn-java-server:${jettyVersion}")
     implementation("org.eclipse.jetty.http2:jetty-http2-server:${jettyVersion}")
@@ -27,10 +14,10 @@ dependencies {
     implementation("org.eclipse.jetty.compression:jetty-compression-server:${jettyVersion}")
     implementation("org.eclipse.jetty.compression:jetty-compression-gzip:${jettyVersion}")
     implementation("org.eclipse.jetty.compression:jetty-compression-brotli:${jettyVersion}") {
-        exclude(brotliDependencyGroup)
+        exclude("com.aayushatharva.brotli4j")
     }
     implementation("org.eclipse.jetty.compression:jetty-compression-zstandard:${jettyVersion}") {
-        exclude(zstdDependencyGroup)
+        exclude("com.github.luben")
     }
 }
 
