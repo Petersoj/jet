37e1758870c03ab24197864588bd8c69a53f70ef
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-03-07 20:00:33 -0700

    Add `Handler` functional interface to `module:server`

---
+++ module/server/src/main/java/net/jacobpeterson/jet/server/handler/Handler.java
@@ -0,0 +1,20 @@
+package net.jacobpeterson.jet.server.handler;
+
+import net.jacobpeterson.jet.server.handle.Handle;
+import org.jspecify.annotations.NullMarked;
+
+/**
+ * {@link Handler} is a {@link FunctionalInterface} for handling a web server request and response using a
+ * given {@link Handle} instance.
+ */
+@NullMarked
+@FunctionalInterface
+public interface Handler {
+
+    /**
+     * Handles a web server request and response.
+     *
+     * @param handle the {@link Handle}
+     */
+    void handle(final Handle handle);
+}
