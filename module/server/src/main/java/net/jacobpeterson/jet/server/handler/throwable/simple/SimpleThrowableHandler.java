0111409813dee962870663723d977c9d9f772cea
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-04-30 12:19:21 -0600

    Use `throwable instanceof StatusException` instead of `getCausalChain()` in `SimpleThrowableHandler`

--- module/server/src/main/java/net/jacobpeterson/jet/server/handler/throwable/simple/SimpleThrowableHandler.java
+++ module/server/src/main/java/net/jacobpeterson/jet/server/handler/throwable/simple/SimpleThrowableHandler.java
@@ -9,7 +9,6 @@ import net.jacobpeterson.jet.server.handler.throwable.ThrowableHandler;
 import org.jspecify.annotations.NullMarked;
 import org.slf4j.event.Level;
 
-import static com.google.common.base.Throwables.getCausalChain;
 import static net.jacobpeterson.jet.common.http.status.Status.INTERNAL_SERVER_ERROR_500;
 import static org.slf4j.event.Level.DEBUG;
 import static org.slf4j.event.Level.ERROR;
@@ -18,7 +17,7 @@ import static org.slf4j.event.Level.ERROR;
  * {@link SimpleThrowableHandler} is a simple {@link ThrowableHandler} implementation that sets
  * {@link Response#setStatusCode(int)} to {@link StatusException#getStatusCode()} or to
  * {@link Status#INTERNAL_SERVER_ERROR_500}, clears {@link Response#getHeaders()}, clears the existing response body
- * streams and calls {@link Response#setBodyString(String)} to {@link Status#toString()}, and logs the {@link Throwable}
+ * and calls {@link Response#setBodyString(String)} to {@link Status#toString()}, and logs the {@link Throwable}
  * with {@link Level#ERROR} if not a {@link StatusException}.
  */
 @NullMarked
@@ -29,19 +28,23 @@ public class SimpleThrowableHandler implements ThrowableHandler {
     public void handle(final Handle handle, final Throwable throwable) {
         final var response = handle.getResponse();
         response.getHeaders().clear();
-        final var statusException = getCausalChain(throwable).stream()
-                .filter(link -> link instanceof StatusException)
-                .findFirst()
-                .map(link -> (StatusException) link);
-        final var statusCode = statusException.map(StatusException::getStatusCode)
-                .orElse(INTERNAL_SERVER_ERROR_500.getCode());
-        final var status = Status.forCode(statusCode);
-        final var statusString = status == null ? statusCode + " Error" : status.toString();
+        final int statusCode;
+        final String statusString;
+        final var isStatusException = throwable instanceof StatusException;
+        if (isStatusException) {
+            statusCode = ((StatusException) throwable).getStatusCode();
+            final var status = Status.forCode(statusCode);
+            statusString = status == null ? statusCode + " Error" : status.toString();
+        } else {
+            final var status = INTERNAL_SERVER_ERROR_500;
+            statusCode = status.getCode();
+            statusString = status.toString();
+        }
         response.responseText(statusCode, statusString);
         // if-statement prevents superfluous `Object[]` creation from varargs.
-        if (statusException.isEmpty() || LOGGER.isDebugEnabled()) {
+        if (isStatusException || LOGGER.isDebugEnabled()) {
             final var request = handle.getRequest();
-            LOGGER.atLevel(statusException.isEmpty() ? ERROR : DEBUG)
+            LOGGER.atLevel(isStatusException ? ERROR : DEBUG)
                     .log("Handler threw with status {} for request: {} {} {}", statusString,
                             request.getVersion(), request.getMethod(), request.getUrl(), throwable);
         }
