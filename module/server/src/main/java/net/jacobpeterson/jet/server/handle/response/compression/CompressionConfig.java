a4ed4b4a320d54642150e173c347951fe42c6c4c
Jacob Peterson <14127217+Petersoj@users.noreply.github.com>
2026-05-31 12:26:08 -0600

    Fix `CompressionConfig` & `MultipartConfig` `DEFAULT` static instance field order in `server` module

--- module/server/src/main/java/net/jacobpeterson/jet/server/handle/response/compression/CompressionConfig.java
+++ module/server/src/main/java/net/jacobpeterson/jet/server/handle/response/compression/CompressionConfig.java
@@ -27,11 +27,6 @@ import static net.jacobpeterson.jet.common.http.header.contentencoding.Compressi
 public class CompressionConfig {
 
     /**
-     * A static {@link CompressionConfig} instance with all default values.
-     */
-    public static final CompressionConfig DEFAULT = builder().build();
-
-    /**
      * An {@link ImmutableList} of {@link Level}s with {@link Level#getLevel()} set to <code>null</code> and in the
      * following order: {@link CompressionType#ZSTANDARD}, {@link CompressionType#BROTLI}, {@link CompressionType#GZIP},
      * {@link CompressionType#DEFLATE}.
@@ -41,6 +36,11 @@ public class CompressionConfig {
             new Level(BROTLI, null),
             new Level(GZIP, null),
             new Level(DEFLATE, null));
+
+    /**
+     * A static {@link CompressionConfig} instance with all default values.
+     */
+    public static final CompressionConfig DEFAULT = builder().build();
 
     /**
      * {@link Level} is an immutable class that represents a {@link CompressionType} mapped to a compression level.
